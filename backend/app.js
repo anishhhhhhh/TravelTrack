26af0ed335866392ecd28c22e239ed87f78a5eb8
Anish Ashtaputre <supravin885@gmail.com>
2023-12-19 00:44:41 +0530

    Add New Mongo Address

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,8 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zbmf9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    // `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zbmf9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    `mongodb+srv://anish:vit123@cluster0.zbmf9.mongodb.net/`
   )
   .then(() => {
     app.listen(process.env.PORT || 5000, () => {
